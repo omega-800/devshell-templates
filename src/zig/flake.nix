0f3a3a7050430ce228a4e88c4a5a67f030bac06d
omega <georgiy.shevoroshkin@ost.ch>
2025-11-16 20:48:25 +0100

    zls overlay

--- src/zig/flake.nix
+++ src/zig/flake.nix
@@ -1,10 +1,25 @@
 {
   description = "zig development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zls-overlay = {
+      url = "github:omega-800/zls-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      nixpkgs,
+      zig-overlay,
+      zls-overlay,
+      ...
+    }:
     let
       systems = nixpkgs.lib.platforms.unix;
       eachSystem =
@@ -20,50 +35,53 @@
           )
         );
       pname = "";
+      version = "0.14.0";
     in
     {
+
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShellNoCC {
-          packages = with pkgs; [
-            zig
-            zls 
-            lldb
+          packages = [
+            zig-overlay.packages.${pkgs.system}."${version}"
+            zls-overlay.packages.${pkgs.system}."${version}"
+            pkgs.lldb
           ];
         };
       });
 
-      /* TODO:
-      packages = eachSystem (
-        pkgs:
-        let
-          fs = pkgs.lib.fileset;
-          root = ./.;
-        in
-        {
-          default = pkgs.buildGoModule {
-            inherit pname;
-            version = "0.0.1";
-            vendorHash = null;
-            src = fs.toSource {
-              inherit root;
-              fileset = fs.intersection (fs.gitTracked root) (
-                fs.unions [
-                  ./go.mod
-                  (fs.fileFilter (f: f.hasExt "go") ./src)
-                  (fs.fileFilter (f: f.hasExt "go") ./.)
-                ]
-              );
+      /*
+        TODO:
+        packages = eachSystem (
+          pkgs:
+          let
+            fs = pkgs.lib.fileset;
+            root = ./.;
+          in
+          {
+            default = pkgs.buildGoModule {
+              inherit pname;
+              version = "0.0.1";
+              vendorHash = null;
+              src = fs.toSource {
+                inherit root;
+                fileset = fs.intersection (fs.gitTracked root) (
+                  fs.unions [
+                    ./go.mod
+                    (fs.fileFilter (f: f.hasExt "go") ./src)
+                    (fs.fileFilter (f: f.hasExt "go") ./.)
+                  ]
+                );
+              };
             };
-          };
-        }
-      );
+          }
+        );
 
-      apps = eachSystem (pkgs: {
-        default = {
-          type = "app";
-          program = "${self.packages.${pkgs.system}.default}/bin/${pname}";
-        };
-      });
+        apps = eachSystem (pkgs: {
+          default = {
+            type = "app";
+            program = "${self.packages.${pkgs.system}.default}/bin/${pname}";
+          };
+        });
       */
     };
 }
